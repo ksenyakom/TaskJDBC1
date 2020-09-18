e26b82b12d6507d6473a27527e23d11cc83a2fdb
ksenys kom <ksenyakom@gmail.com>
2020-09-18 11:04:02 +0300

    test

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,26 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.util.Util;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class Main {
+    public static String CREATE = "CREATE TABLE userdb.users " +
+            "(id serial NOT NULL primary key, " +
+            "name varchar(45) NOT NULL, " +
+            "last_name varchar(45) NOT NULL, " +
+            "age TINYINT NOT NULL)";
+
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        Connection connection = Util.getConnection();
+        try (PreparedStatement statement = connection.prepareStatement(CREATE)) {
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        // создание таблицы
+
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,38 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private final static String DB_URL =
+            "jdbc:mysql://localhost:3306/userdb?useUnicode=true&serverTimezone=UTC&useSSL=false";
+    private final static String USER = "root";
+    private final static String PASS = "Middlejav1984";
+
+    private static Connection connection;
+
+    private final static String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
+
+    static {
+        try {
+            System.out.println("Regestering jdbc driver...");
+            Class.forName(DB_DRIVER);
+        } catch (ClassNotFoundException e) {
+            System.err.println("Exception while regestering jdbc driver");
+            e.printStackTrace();
+        }
+
+        try {
+            System.out.println("Connection to database...");
+            connection = DriverManager.getConnection(DB_URL, USER, PASS);
+        } catch (SQLException e) {
+            System.err.println("Exception while connection to database");
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection getConnection() {
+        return connection;
+    }
 }
